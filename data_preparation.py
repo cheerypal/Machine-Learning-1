27c8483ef5e8ac5373cdc89611180c7f541b1cc9
yaseenjamal <63450896+yaseenjamal@users.noreply.github.com>
2020-10-13 14:16:13 +0100

    Fix randomization

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,18 @@
 import random, os
+
+def seed():
+    return 0.31415926535898
+
 def randomize_data():
     if os.path.exists("randomized_data"):
-        print("Randomized training training_data already exists.")
+        print("Randomized data already exists.")
     else:
         os.mkdir("randomized_data")
         for filename in os.listdir("training_data"):
             print("Randomizing " + filename)
-            with open("training_data/" + filename, "r") as source:
-                data = [(random.random(), line) for line in source]
-            data.sort()
-            with open("randomized_data/" + filename[0:len(filename)-4] + "_randomized.csv", "w") as target:
-                for _, line in data:
+            file = open("training_data/" + filename, "r")
+            training_data = file.readlines()
+            random.shuffle(training_data, seed)
+            with open("randomized_data/" + filename[0:len(filename) - 4] + "_randomized.csv", "w") as target:
+                for line in training_data:
                     target.write(line)
