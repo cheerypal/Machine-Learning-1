30e7ea892b8e6029f7c13c156cd2d2a7493caaab
Euan Gordon <ejg9@hw.ac.uk>
2020-10-13 14:26:12 +0100

    Add header

--- data_preparation.py
+++ data_preparation.py
@@ -1,7 +1,7 @@
 import random, os
 
 def seed():
-    return 0.31415926535898
+    return 0.1
 
 def randomize_data():
     if os.path.exists("randomized_data"):
@@ -12,7 +12,10 @@ def randomize_data():
             print("Randomizing " + filename)
             file = open("training_data/" + filename, "r")
             training_data = file.readlines()
-            random.shuffle(training_data, seed)
+            header = training_data[0]
+            data = training_data[1:]
+            random.shuffle(data, seed)
+            data.insert(0, header)
             with open("randomized_data/" + filename[0:len(filename) - 4] + "_randomized.csv", "w") as target:
-                for line in training_data:
+                for line in data:
                     target.write(line)
