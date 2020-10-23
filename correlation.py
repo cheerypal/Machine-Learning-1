bcf2077f03db552af5d48babeda82cf4f622c71c
Euan Gordon <ejg9@hw.ac.uk>
2020-10-23 13:12:45 +0100

    correlation.py

--- correlation.py
+++ correlation.py
@@ -1,13 +1,22 @@
+
 import pandas
 
 # Read training data and associated labels
 data = pandas.read_csv("randomized_data/x_train_gr_smpl_randomized.csv")
+ARR = ["speed-20", "speed-30", "speed-50", "speed-60", "speed-70", "left-t", "right-t", "pedestrian", "children",
+       "cycle"]
+for x in range(0, 10):
+    labels = pandas.read_csv("randomized_data/y_train_smpl_" + str(x) + "_randomized.csv")
+    labels = labels.rename(columns={"0": ARR[x]})
+    data = pandas.concat([data, labels], axis=1)
 
-for x in range(0,10):
-    labels = pandas.read_csv("randomized_data/y_train_smpl_"+str(x)+"_randomized.csv")
-    labels.rename(columns = {0:2304+x},inplace=True)
-    print(labels)
-    data = pandas.concat([data,labels],axis=1)
-    print(data)
+print(data)
 
-#print(data.corr())
+corr = abs(data.corr())
+
+for x in range(0, 10):
+
+    sortedCorr = corr[ARR[x]][0:2304].sort_values(ascending=False)
+    sortedCorr = sortedCorr[0:10].index
+    print("#### " + ARR[x] + " ####")
+    print(pandas.array(sortedCorr))
