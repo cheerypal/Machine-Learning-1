76559cac40fc75f4d888e1461e13200f59a51606
amaan-akram <amaan7akram@gmail.com>
2020-10-31 12:40:39 +0000

    fix indentation data prep

--- data_preparation.py
+++ data_preparation.py
@@ -11,14 +11,14 @@ def randomize_data():
         print("Randomized data already exists.")
     else:
         os.mkdir("randomized_data")
-        for filename in os.listdir("training_data"):
-            print("Randomizing " + filename)
-            file = open("training_data/" + filename, "r")
-            training_data = file.readlines()
-            header = training_data[0]
-            data = training_data[1:]
-            random.shuffle(data, seed)
-            data.insert(0, header)
-            with open("randomized_data/" + filename[0:len(filename) - 4] + "_randomized.csv", "w") as target:
-                for line in data:
-                    target.write(line)
+    for filename in os.listdir("training_data"):
+        print("Randomizing " + filename)
+        file = open("training_data/" + filename, "r")
+        training_data = file.readlines()
+        header = training_data[0]
+        data = training_data[1:]
+        random.shuffle(data, seed)
+        data.insert(0, header)
+        with open("randomized_data/" + filename[0:len(filename) - 4] + "_randomized.csv", "w") as target:
+            for line in data:
+                target.write(line)
